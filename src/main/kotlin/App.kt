7089aefafffa043219ae23204450cb35750734c9
Nicolas Teixeira <nicolasteixeira3856@outlook.com>
2022-02-28 14:30:49 -0300

    initiating with kotlin

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,26 @@
+fun main() {
+    /*var x = 1
+    println("Before the loop. x = $x")
+    while (x < 4) {
+        println("In the loop. x = $x")
+        x = x + 1
+    }
+    println("After the loop. x = $x")*/
+    /*val x = 3;
+    val y = 1;
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")*/
+//    var x = 1;
+//    while (x < 3) {
+//        print(if (x == 1) "Yab" else "Dab")
+//        print("ba")
+//        x += 1
+//    }
+//    if (x == 3) println("Do")
+
+    var x = 10
+    while (x > 1) {
+        x = x - 1
+        if (x < 3) println("small x")
+    }
+}
